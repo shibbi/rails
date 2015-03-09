15c9a0d9406f244d43a1a672ff1f6c4b94e31f89
shibbi <shibo.fang@gmail.com>
2015-03-08 18:58:41 -0700

    Complete tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(', ')
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map { |tag| tag.strip.downcase }.uniq
+    self.tags = tag_names.map { |tag| Tag.find_or_create_by(name: tag) }
+  end
 end
